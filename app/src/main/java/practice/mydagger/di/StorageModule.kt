9bcc0f932b7d1ec593436a3ce97659575f174820
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2020-01-18 18:34:34 +0530

    Use @Provides instead of @Binds

--- app/src/main/java/practice/mydagger/di/StorageModule.kt
+++ app/src/main/java/practice/mydagger/di/StorageModule.kt
@@ -1,16 +1,22 @@
 package practice.mydagger.di
 
-import dagger.Binds
+import android.content.Context
 import dagger.Module
+import dagger.Provides
 import practice.mydagger.storage.SharedPreferencesStorage
 import practice.mydagger.storage.Storage
 
 // Tells Dagger this is a Dagger module
-// Because of @Binds, StorageModule needs to be an abstract class
 @Module
-abstract class StorageModule {
+class StorageModule {
 
-    // Makes Dagger provide SharedPreferencesStorage when a Storage type is requested
-    @Binds
-    abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+    // @Provides tell Dagger how to create instances of the type that this function
+    // returns (i.e. Storage).
+    // Function parameters are the dependencies of this type (i.e. Context).
+    @Provides
+    fun provideStorage(context: Context): Storage {
+        // Whenever Dagger needs to provide an instance of type Storage,
+        // this code (the one inside the @Provides method) will be run.
+        return SharedPreferencesStorage(context)
+    }
 }
